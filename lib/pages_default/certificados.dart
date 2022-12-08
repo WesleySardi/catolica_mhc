8801a73e88e4c8c1fb7a031097de0d3404885a9e
Wesley Sardi <wesley.sardi@gmail.com>
2022-12-08 04:33:58 -0300

    Certificados aprimorados com modal de deleção.

--- lib/pages_default/certificados.dart
+++ lib/pages_default/certificados.dart
@@ -18,6 +18,57 @@ class Certificados extends StatefulWidget {
 
 class _CertificadosState extends State<Certificados> {
 
+  void ShowModal2(BuildContext context) {
+    //code to execute on button press
+    //botao aparece as coisas
+    showModalBottomSheet<void>(
+      context: context,
+
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+            top: Radius.circular(50)
+        ),
+      ),
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          child: Center(
+            child: FractionallySizedBox(
+              heightFactor: 0.8,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 2,
+                          shape: StadiumBorder(),
+                          minimumSize: Size(300, 43),
+                          maximumSize: Size(300, 43),
+                          backgroundColor: Colors.red[900]),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(Icons.add_chart_sharp),
+                          Text("Deletar certificado     ", style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                      onPressed: () {
+
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   late List<String> tituloList = <String>[];
   late List<String> instituicaoList = <String>[];
   late List<String> imgList = <String>[];
@@ -303,6 +354,7 @@ class _CertificadosState extends State<Certificados> {
                                                         selectedOption2 = index;
                                                         selectedOption = imgList.length+1;
 
+                                                        ShowModal2(context);
 
                                                     });
                                                   }
